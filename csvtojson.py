6cb0c3c12ca047201161a74c64c51fae1cf94d22
AS20017625 <ashish.sharma67@wipro.com>
2018-12-28 18:29:19 +0530

    added support to read heading from csv and code optimization

--- csvtojson.py
+++ csvtojson.py
@@ -1,33 +1,32 @@
 import csv
 import json
-def parseCSV(filen,pkey=None,fields=None):
-    f=open(filen,"r");
-    #title = f.readlines();
-    reader = csv.DictReader(f,fieldnames=fields);
-    #f.close();
-    di = {}
-    if pkey not in fields:
-        return None;
+def parseCSV(filen,pkey=None,fields=None,header=False):
+    with open(filen,"r") as f:
+        if header:
+            fields = tuple(f.readline().strip().split(","))
+            print fields
+        reader = csv.DictReader(f,fieldnames=fields);
+        di = {}
+        if pkey not in fields:
+            print("Key has to be in the header/fields.")
+            return None;
 
-    for row in reader:
-        temp = {}
-        for i in fields:
-            if i == pkey:
-                continue
-            else:
-                temp[i]=row[i]
-        di.update({row[pkey]:temp})
-        #di[row[pkey]]=temp
-        #print temp
-
+        for row in reader:
+            temp = {}
+            print row
+            for i in fields:
+                if i == pkey:
+                    continue
+                else:
+                    temp[i]=row[i]
+            di.update({row[pkey]:temp})
     return di
 
 def main():
-    r = parseCSV("./f1.csv",pkey='k',fields=('k','a','b','c'))
+    r = parseCSV("./f1.csv",pkey='k2',header=True)
     r = json.dumps([r])
     with open("./parsedCSV.json","w") as f:
         f.write(r)
-    print r
 
 if __name__=="__main__":
     main()
